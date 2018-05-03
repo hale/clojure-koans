2245a156adc9d95d17c68adae325fe9bc6d520c6
Philip Hale <philip@pghale.com>
2018-05-03 05:06:09 +0100

    First four chapters, chap 3 with lazy-seq

--- src/koans/03_lists.clj
+++ src/koans/03_lists.clj
@@ -1,45 +1,65 @@
 (ns koans.03-lists
   (:require [koan-engine.core :refer :all]))
 
+(def characters (map #(-> % char str symbol keyword) (range)))
+
+(def a-to-z (take 26 (drop (.indexOf characters :a) characters)))
+
+;; (def a-to-z (->> characters (drop (.indexOf characters :a)) (take 26)))
+
+(def a-to-z-cycle (cycle a-to-z))
+
 (meditations
   "Lists can be expressed by function or a quoted form"
-  (= '(__ __ __ __ __) (list 1 2 3 4 5))
+  (= '(1 2 3 4 5) (list 1 2 3 4 5))
+
+  "BONUS - or generate the list"
+  (= (map inc (range 5)) (list 1 2 3 4 5))
 
   "They are Clojure seqs (sequences), so they allow access to the first"
-  (= __ (first '(1 2 3 4 5)))
+  (= 1 (first '(1 2 3 4 5)))
 
   "As well as the rest"
-  (= __ (rest '(1 2 3 4 5)))
+  (= '(2 3 4 5) (rest '(1 2 3 4 5)))
 
   "Count your blessings"
-  (= __ (count '(dracula dooku chocula)))
+  (= 3 (count '(dracula dooku chocula)))
 
   "Before they are gone"
-  (= __ (count '()))
+  (= 0 (count '()))
 
   "The rest, when nothing is left, is empty"
-  (= __ (rest '(100)))
+  (= '() (rest '(100)))
 
   "Construction by adding an element to the front is easy"
-  (= __ (cons :a '(:b :c :d :e)))
+  (= '(:a :b :c :d :e) (cons :a '(:b :c :d :e)))
+
+  " TAKE 2! Construction by adding an element to the front is easy"
+  (= (take 5 a-to-z) (cons :a '(:b :c :d :e)))
 
   "Conjoining an element to a list isn't hard either"
-  (= __ (conj '(:a :b :c :d) :e))
+  (= '(:e :a :b :c :d) (conj '(:a :b :c :d) :e))
+
+  " TAKE 2! Conjoining an element to a list isn't hard either"
+  (= (cons :e (take 4 a-to-z)) (conj '(:a :b :c :d) :e))
 
   "You can use a list like a stack to get the first element"
-  (= __ (peek '(:a :b :c :d :e)))
+  (= :a (peek '(:a :b :c :d :e)))
 
   "Or the others"
-  (= __ (pop '(:a :b :c :d :e)))
+  (= '(:b :c :d :e) (pop '(:a :b :c :d :e)))
+
+  "TAKE 2! Or the others"
+  (= (->> a-to-z (take 5) (drop 1)) (pop '(:a :b :c :d :e)))
 
   "But watch out if you try to pop nothing"
-  (= __ (try
-          (pop '())
-          (catch IllegalStateException e
-            "No dice!")))
+  (= "No dice!" (try
+                  (pop '())
+                  (catch IllegalStateException e
+                    "No dice!")))
 
   "The rest of nothing isn't so strict"
-  (= __ (try
+  (= '() (try
           (rest '())
           (catch IllegalStateException e
             "No dice!"))))

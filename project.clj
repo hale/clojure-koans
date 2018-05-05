e7c70d1c638b34356088281e8e17ee92f4079a2b
Philip Hale <philip@pghale.com>
2018-05-05 21:51:14 +0100

    chap 9

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojure-koans "0.5.1-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [koan-engine "0.2.4"]]
+                 [koan-engine "0.2.4"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :dev-dependencies [[lein-koan "0.1.4"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.4"]]}}
   :repl-options {:init-ns koan-engine.runner
